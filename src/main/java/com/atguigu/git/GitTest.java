0c146f154c3b562a2f0283c656c477d10077069b
IU <IU@jinan.com>
2023-05-25 14:00:32 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Lu
+ * @create 2023-05-25 13:53
+ */
+public class GitTest {
+	public static void main(String[] args) {
+		System.out.println("hello git");
+	}
+}
